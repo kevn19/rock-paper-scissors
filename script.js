a75d64e0a3e0c2cbf9a8492ae3cc6f2e8d9b38ad
Kevin Nguyen <kevinn.nguyen19@gmail.com>
2022-02-22 11:03:55 -0500

    Updated game to include GUI

--- script.js
+++ script.js
@@ -15,26 +15,63 @@ function playRound(playerSelection, computerPlay) {
     switch (true) {
         case playerSelection == computerPlay:
             tie++
-            return "Its a Tie!"
+            result.textContent = `Tie! You Both Chose ${playerSelection}`
             break;
         case playerSelection === "Rock" && computerPlay === "Scissors":
         case playerSelection === "Paper" && computerPlay === "Rock":
         case playerSelection === "Scissors" && computerPlay === "Paper":
             playerScore++    
-            return "You Win!"
+            result.textContent = `${playerSelection} Beats ${computerPlay}`   
             break;
         case playerSelection === "Scissors" && computerPlay === "Rock":
         case playerSelection === "Rock" && computerPlay === "Paper":
         case playerSelection === "Paper" && computerPlay === "Scissors":
             computerScore++
-            return "You lose!" 
+            result.textContent = `${computerPlay} Beats ${playerSelection}` 
             break;
     }
+    if (playerSelection == "Rock"){
+        playerChoice.className = "far fa-hand-rock"
+    }else if (playerSelection == "Paper"){
+        playerChoice.className = "far fa-hand-paper"
+    }else if (playerSelection == "Scissors"){
+        playerChoice.className = "far fa-hand-scissors"
+    }
+    if (computerPlay == "Rock"){
+        cpuChoice.className = "far fa-hand-rock"
+    }else if (computerPlay == "Paper"){
+        cpuChoice.className = "far fa-hand-paper"
+    }else if (computerPlay == "Scissors"){
+        cpuChoice.className = "far fa-hand-scissors"
+    }
+    gameScore()
+    gameOver()
+
 }
 
 let computerScore = 0
 let playerScore = 0
 let tie = 0
+
+function gameScore(){
+    let pScore = document.querySelector("#playerScore");
+    pScore.textContent = playerScore;
+    let cScore = document.querySelector("#cpuScore");
+    cScore.textContent = computerScore;
+}
+
+function gameOver(){
+    if (playerScore==5){
+        computerScore=0;
+        playerScore=0
+        result.textContent = "Congratulations! You Won!"
+    }else if (computerScore==5){
+        computerScore=0;
+        playerScore=0
+        result.textContent = "Sorry! You Lost!"
+
+    }
+}
 
 function game() {
     for (i=0; i<5; i++) {
@@ -45,4 +82,39 @@ function game() {
     
 }
 
-game()
+let rock = document.querySelector("#rock")
+let paper = document.querySelector("#paper")
+let scissors = document.querySelector("#scissors");
+
+let result = document.querySelector(".result")
+
+
+rock.addEventListener("click", () => {
+    playRound("Rock", computerPlay())
+})
+paper.addEventListener("click", () => {
+    playRound("Paper", computerPlay())
+})
+
+scissors.addEventListener("click", () => {
+    playRound("Scissors", computerPlay())
+})
+
+function displayChoice() {
+    switch(true){
+        case playerSelection == "Rock":
+            playerChoice.className = "far fa-hand-rock"
+        break;
+        case playerSelection == "Paper":
+            playerChoice.className = "far fa-hand-paper"
+            break;
+        case playerSelection == "Scissors":
+            playerChoice.className = "far fa-hand-scissors"
+        break;
+    }
+}
+let playerChoice = document.querySelector("#playerChoice")
+
+
+let cpuChoice = document.querySelector("#cpuChoice")
+
